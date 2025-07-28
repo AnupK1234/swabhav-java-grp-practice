c5e98c6fec2ab5724707af7645aa0a319e635185
Anup Khismatrao <khismatrao2003@gmail.com>
2025-07-28 21:56:20 +0530

    Refactor: callback, promises

--- Javascript Practice/day1/callback.js
+++ Javascript Practice/day1/callback.js
@@ -1,16 +1,34 @@
-async function callAPI() {
-    const data = await fetch("https://randomuser.me/api/");
-    const data2 = await data.json();
-    // console.log(data2.results[0]);
-    
-    greetUser(data2.results[0]);
+function callAPI(callback) {
+  fetch("https://randomuser.me/api/")
+    .then(res => res.json())
+    .then(data => {
+      // Pass the user data to the callback
+      callback(data.results[0]);
+    })
+    .catch(err => console.error("Error:", err));
 }
 
 function greetUser(userData) {
-    console.log("Welcome " + userData.name.first);
+  console.log("Welcome " + userData.name.first);
 }
 
-callAPI();
+// Pass greetUser as a callback
+callAPI(greetUser);
+
+
+// async function callAPI() {
+//     const data = await fetch("https://randomuser.me/api/");
+//     const data2 = await data.json();
+//     // console.log(data2.results[0]);
+    
+//     greetUser(data2.results[0]);
+// }
+
+// function greetUser(userData) {
+//     console.log("Welcome " + userData.name.first);
+// }
+
+// callAPI();
 
 
 // callback hell - akshay saini  namaste js async await
